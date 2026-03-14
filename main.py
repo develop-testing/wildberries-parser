b9894a094709fc9f3f14b92c0bedcac47812bbd1
Aleksey <devmailbox111@gmail.com>
2026-03-14 14:37:08 +0500

    change goods object: remove data list to product ids list

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ from __future__ import annotations
 from typing import Protocol
 from dataclasses import dataclass
 from promise import Promise
-import requests
+
+from DrissionPage import ChromiumPage, ChromiumOptions
 
 
 @dataclass(slots=True)
 class ProductPrint:
+    descr: str
+    images: list[str]
+    characters: list[str]
+    raiting: int
+    reviews_count: int
     link: str
+    articul: str
     name: str
+    price: str
+    seller_name: str
+    seller_link: str
+    sizes: list[str]
+    quantity: int
 
 
 @dataclass(slots=True)
@@ -26,7 +38,7 @@ class Goods(Protocol):
 @dataclass(slots=True, frozen=True)
 class GoodsOfSearchQuery(Goods):
     query: str
-    data: list[dict[str, str]]
+    products: list[int]
 
     def __post_init__(self):
         if len(self.query) > 500:
@@ -35,32 +47,21 @@ class GoodsOfSearchQuery(Goods):
         if not isinstance(self.query, str):
             raise ValueError("query of goods is not string")
         
-        if self.data:
-            for item in self.data:
-                for value in item.values():
-                    if not isinstance(value, str):
-                        raise ValueError("value of goods is not string")
+        if self.products:
+            for value in self.products:
+                if not isinstance(value, int):
+                    raise ValueError("product id is not int")
 
-                    if len(value) > 3000:
-                        raise ValueError("value of goods items is too long")
+                if len(value) > 256:
+                    raise ValueError("product id is too long")
 
 
     def print(self) -> GoodsPrint:
         printout = GoodsPrint(
             query=self.query,
             count=len(self.data),
-            products=[]
+            products=self.products
         )
-
-        for item in self.data:
-            printout.products.append(
-                ProductPrint(
-                    link=item['link'],
-                    articul=item['articul'],
-                    name=item['name'],
-                    price=item['price'],
-                )
-            )
 
         return printout
 
@@ -75,27 +76,22 @@ class WildberriesScrappedGoods(Goods):
     @staticmethod
     def new(query: str) -> Promise[WildberriesScrappedGoods]:
         def executor(resolve, reject):
+            options = ChromiumOptions()
+            options.headless(True)  # Это скроет окно браузера
+            page = ChromiumPage(addr_or_opts=options)
+
+            url = "https://www.wildberries.ru/__internal/u-search/exactmatch/ru/common/v18/search?ab_testing=false&appType=1&curr=rub&dest=-5818883&hide_vflags=4294967296&lang=ru&page=1&query=пальто из натуральной шерсти&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false"
+
             try:
-                data = requests.get(
-                    "https://www.wildberries.ru/catalog/444246710/detail.aspx",
-                    headers={
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-                        'Accept-Encoding': 'gzip, deflate, br',
-                        'DNT': '1',
-                        'Connection': 'keep-alive',
-                        'Upgrade-Insecure-Requests': '1',
-                        'Sec-Fetch-Dest': 'document',
-                        'Sec-Fetch-Mode': 'navigate',
-                        'Sec-Fetch-Site': 'none',
-                        'Sec-Fetch-User': '?1',
-                        'Cache-Control': 'max-age=0',
-                    }
-                ).text
-                print(data)
-            except Exception as e:
-                reject(e)
+                page.get(url)
+
+                items = page.json["products"]
+
+                for item in items:
+                    print(item)
+                    break
+            finally:
+                page.quit()
 
         return (
             Promise(executor)
