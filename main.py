c24d728f901f5abaff1e53af8a15a8cf6abc500f
Aleksey <devmailbox111@gmail.com>
2026-03-14 21:52:30 +0500

    added wildberries product detail

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 from typing import Protocol
 from dataclasses import dataclass, asdict
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from goods.scrapped_goods import WildberriesScrappedGoods
 from goods.fk_goods import FakeGoods
 from goods.json_chached_goods import JsonFileCachedGoods
 
 from product.fk_product import FakeProduct
+from product.wildberries_product import WildberriesProduct
 
 goods = JsonFileCachedGoods.new(
     WildberriesScrappedGoods.new("пальто из натуральной шерсти"),
@@ -15,9 +17,17 @@ goods = JsonFileCachedGoods.new(
 
 printout = goods.print()
 
-for id in printout.products:
-    print(
-        FakeProduct
-            .new(id)
-            .print()
-    )
+import time
+start = time.time()
+
+with ThreadPoolExecutor(max_workers=3) as executor:
+    futures = [
+        executor.submit(WildberriesProduct.new(id).print)
+        for id in [6489086, 11275582, 481144243]
+    ]
+
+    for future in as_completed(futures):
+        result = future.result()
+        print(result)
+
+print(f"Общее время: {time.time() - start:.2f} сек")
---
+++ product/wildberries_product.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from DrissionPage import ChromiumPage, ChromiumOptions, SessionPage
+from time import sleep
+
+from .products import Products
+from .products import ProductData
+from .product import Product
+
+
+@dataclass(slots=True, frozen=True)
+class WildberriesProduct(Products):
+    origin: int
+
+    def print(self) -> ProductData:
+        try:
+            data = self.origin.print()
+
+            articul_str = str(data.articul)
+            vol_num = 0
+            part_num = 0
+
+            match len(articul_str):
+                case 7:
+                    vol_num = articul_str[:2]
+                    part_num = articul_str[:4]
+                case 8:
+                    vol_num = articul_str[:3]
+                    part_num = articul_str[:5]
+                case 9:
+                    vol_num = articul_str[:4]
+                    part_num = articul_str[:6]
+
+        
+            options = ChromiumOptions()
+            options.headless(True)  # hide window
+            web_page = ChromiumPage(addr_or_opts=options)
+        
+            url = f"https://mns-basket-cdn-02.geobasket.net/vol{vol_num}/part{part_num}/{articul_str}/info/ru/card.json"
+
+            web_page.get(url)
+
+            first_data = web_page.json
+
+            web_page\
+                .get(f"https://www.wildberries.ru/__internal/u-card/cards/v4/detail?appType=1&curr=rub&dest=-1257786&spp=30&hide_vflags=4294967296&ab_testing=false&lang=ru&nm={articul_str}")
+            
+            second_data = web_page.json
+
+            max_images = 99
+            image_counter = 1
+            images = []
+
+            session = SessionPage()
+
+            while image_counter <= max_images:
+                url = f"https://mns-basket-cdn-02.geobasket.net/vol{vol_num}/part{part_num}/{articul_str}/images/big/{image_counter}.webp"
+
+                result = session.session.head(url)
+
+                if result.status_code == 200:
+                    images.append(url)
+                    image_counter += 1
+                    sleep(0.3)
+                    continue
+                
+                break
+
+            data.price = next((s.get('price', {}).get('product', 0) // 100 for s in second_data['products'][0].get('sizes', []) if s.get('price')), 0)
+            data.name = first_data["imt_name"]
+            data.descr = first_data["description"] 
+            data.images = images
+            data.characters = first_data["options"]
+            data.raiting = int(second_data["products"][0]["reviewRating"])
+            data.reviews_count = int(second_data["products"][0]["nmFeedbacks"])
+            data.link = f"https://www.wildberries.ru/catalog/{data.articul}/detail.aspx"
+            data.seller_name = first_data["selling"]["brand_name"]
+            data.seller_link = f"https://www.wildberries.ru/brands/{data.seller_name}"
+            data.sizes = [size['name'] for size in second_data['products'][0]['sizes']]
+            data.quantity = int(second_data['products'][0]['totalQuantity'])
+
+            return data
+
+        except Exception as e:
+            return data
+        
+        finally:
+            web_page.quit()
+    
+    @staticmethod
+    def new(id: int) -> WildberriesProduct:
+        return WildberriesProduct(
+            Product(id, ProductData.empty())
+        )
