b0e4e91907e2cbc2ab9310b6dd5f2a4d2e5d78ec
Aleksey <devmailbox111@gmail.com>
2026-03-15 17:35:04 +0500

    change webpage to session for WildberriesProduct

--- main.py
+++ main.py
@@ -24,13 +24,26 @@ printout = goods.print()
 
 products_to_update = []
 
+products_to_update.append(
+    JsonCachedProduct(
+        WildberriesProduct
+            .new(204971561, '1.1000.3c0234109f2f4703b35e7303f1c59d5a.MTV8OTUuMjYuNjQuMjI5fE1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NDsgcnY6MTQwLjApIEdlY2tvLzIwMTAwMTAxIEZpcmVmb3gvMTQwLjB8MTc3NDYwNzU0MHxyZXVzYWJsZXwyfGV5Sm9ZWE5vSWpvaUluMD18MHwzfDE3NzQwMDI3NDB8MQ==.MEQCIHbC2hNqZY7t0TB3PIRdJ9DUJAcCfL5S7hnyMmFToN9dAiA51lbY7zSowsJJqAz8UZFqWDbdHsNgYWcZ2uY1vQ6v3A=='),
+        f"cache/products/product_{204971561}.json"
+    ).print()
+)
+
+print(products_to_update)
+
+"""
 for id in printout.products:
     products_to_update.append(
         JsonCachedProduct(
-            FakeProduct.new(id), f"cache/products/product_{id}.json"
+            WildberriesProduct
+            .new(id), f"cache/products/product_{id}.json"
         ).print()
     )
-
+"""
+    
 table = PandasDataTable.new("result/all_product.xlsx").update(
     [
         DataTableRow(
--- product/wildberries_product.py
+++ product/wildberries_product.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from DrissionPage import ChromiumPage, ChromiumOptions, SessionPage  # type: ignore
+from DrissionPage import ChromiumPage, ChromiumOptions, SessionPage, SessionOptions # type: ignore
 from time import sleep
 
 from .products import Products
@@ -11,9 +11,10 @@ from .product import Product
 @dataclass(slots=True, frozen=True)
 class WildberriesProduct(Products):
     origin: Products
+    x_wbaas_token: str
 
     def print(self) -> ProductData:
-        try:
+        #try:
             data = self.origin.print()
 
             articul_str = str(data.articul)
@@ -31,39 +32,33 @@ class WildberriesProduct(Products):
                     vol_num = articul_str[:4]
                     part_num = articul_str[:6]
 
-            options = ChromiumOptions()
-            options.set_argument("--no-sandbox")
-            options.set_argument("--disable-blink-features=AutomationControlled")
-            options.set_user_agent(
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-            )
-            options.set_argument("--disable-notifications")
-            options.set_argument("--disable-extensions")
-            options.set_argument("--headless=new")
-            web_page = ChromiumPage(addr_or_opts=options)
+            page = SessionPage()
 
-            url = f"https://mns-basket-cdn-02.geobasket.net/vol{vol_num}/part{part_num}/{articul_str}/info/ru/card.json"
+            page.session.headers.update({
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+                'Referer': 'https://www.wildberries.ru',
+                'Accept': '*/*',
+                'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+            })
 
-            web_page.get(url)
+            page.session.cookies.update({'x_wbaas_token': self.x_wbaas_token})
 
-            first_data = web_page.json
+            page.get(f"https://mns-basket-cdn-02.geobasket.net/vol{vol_num}/part{part_num}/{articul_str}/info/ru/card.json")
 
-            web_page.get(
-                f"https://www.wildberries.ru/__internal/u-card/cards/v4/detail?appType=1&curr=rub&dest=-1257786&spp=30&hide_vflags=4294967296&ab_testing=false&lang=ru&nm={articul_str}"
-            )
+            first_data = page.json
 
-            second_data = web_page.json
+            page.get(f"https://www.wildberries.ru/__internal/u-card/cards/v4/detail?appType=1&curr=rub&dest=-1257786&spp=30&hide_vflags=4294967296&ab_testing=false&lang=ru&nm={articul_str}")
+
+            second_data = page.json
 
             max_images = 99
             image_counter = 1
             images = []
 
-            session = SessionPage()
-
             while image_counter <= max_images:
                 url = f"https://mns-basket-cdn-02.geobasket.net/vol{vol_num}/part{part_num}/{articul_str}/images/big/{image_counter}.webp"
 
-                result = session.session.head(url)
+                result = page.session.head(url)
 
                 if result.status_code == 200:
                     images.append(url)
@@ -90,19 +85,19 @@ class WildberriesProduct(Products):
             data.raiting = int(second_data["products"][0]["reviewRating"])
             data.reviews_count = int(second_data["products"][0]["nmFeedbacks"])
             data.link = f"https://www.wildberries.ru/catalog/{data.articul}/detail.aspx"
-            data.seller_name = first_data["selling"]["brand_name"]
+            data.seller_name = (first_data.get("selling") or {}).get("brand_name") or ""
             data.seller_link = f"https://www.wildberries.ru/brands/{data.seller_name}"
             data.sizes = [size["name"] for size in second_data["products"][0]["sizes"]]
             data.quantity = int(second_data["products"][0]["totalQuantity"])
 
             return data
 
-        except Exception as e:
-            return data
-
-        finally:
-            web_page.quit()
+        #except Exception as e:
+        #    return data
 
     @staticmethod
-    def new(id: int) -> WildberriesProduct:
-        return WildberriesProduct(Product(id, ProductData.empty()))
+    def new(id: int, x_wbaas_token: str) -> WildberriesProduct:
+        return WildberriesProduct(
+            Product(id, ProductData.empty()),
+            x_wbaas_token
+        )
