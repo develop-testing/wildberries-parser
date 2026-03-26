030ea95cfe37cbcac605993161b5f242668936b0
Aleksey <devmailbox111@gmail.com>
2026-03-26 16:59:16 +0500

    added search products

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ from fastapi.responses import HTMLResponse
 from dataclasses import dataclass
 from html_page.mstache_html_page import MstacheHtmlPage
 from product.fk_product import FakeProduct
-
+from product.wildberries_product import WildberriesProduct
+from goods.wildberries_goods import WildberriesGoods
 
 app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    products = [
-        FakeProduct.new(1),
-        FakeProduct.new(2),
-        FakeProduct.new(3),
-        FakeProduct.new(4),
-    ]
+async def home(request: Request, query = "", marketplace = []):
+    if marketplace == "wildberries":
+        goods_query = WildberriesGoods.new(
+            query,
+            '1.1000.3c0234109f2f4703b35e7303f1c59d5a.MTV8OTUuMjYuNjQuMjI5fE1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NDsgcnY6MTQwLjApIEdlY2tvLzIwMTAwMTAxIEZpcmVmb3gvMTQwLjB8MTc3NDYwNzU0MHxyZXVzYWJsZXwyfGV5Sm9ZWE5vSWpvaUluMD18MHwzfDE3NzQwMDI3NDB8MQ==.MEQCIHbC2hNqZY7t0TB3PIRdJ9DUJAcCfL5S7hnyMmFToN9dAiA51lbY7zSowsJJqAz8UZFqWDbdHsNgYWcZ2uY1vQ6v3A=='
+        )
+
+    view_products = products = []
+
+    wb_goods_ids = goods_query.print().products
+
+    for id in wb_goods_ids:
+        view_products.append(FakeProduct.new(id))
 
     page = MstacheHtmlPage.new('index.html')
 
